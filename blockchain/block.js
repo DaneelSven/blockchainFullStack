e4d67abaecddcbea12cac417cb3140126cbff4ea
sven <sven4696@gmail.com>
2020-05-29 23:15:14 +0200

    workign with transactions and wallets

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,7 +1,7 @@
 //static functions when you use this function on the class itself and not the instance of a class
 const hexToBinary = require("hex-to-binary");
 const { GENESIS_DATA, MINE_RATE } = require("../config");
-const cryptoHash = require("../util/crypto-hash");
+const { cryptoHash } = require("../util/");
 
 class Block {
   constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 const MINE_RATE = 1000; // set in milliseconds which equals to 1 second
-const INITIAL_DIFFICULTY = 3
+const INITIAL_DIFFICULTY = 3;
 
 const GENESIS_DATA = {
-    timestamp: 1,
-    lastHash: '----',
-    hash: 'hash-one',
-    difficulty: INITIAL_DIFFICULTY,
-    nonce: 0,
-    data: []
-}
+  timestamp: 1,
+  lastHash: "----",
+  hash: "hash-one",
+  difficulty: INITIAL_DIFFICULTY,
+  nonce: 0,
+  data: [],
+};
+
+const STARTING_BALANCE = 1000;
 
 module.exports = {
-    GENESIS_DATA,
-    MINE_RATE
-}
+  GENESIS_DATA,
+  MINE_RATE,
+  STARTING_BALANCE,
+};
